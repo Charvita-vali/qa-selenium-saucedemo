c3c6f3fb34841bcd468dc9d46ba3acf98ce8bfcf
Charvita-vali <charvita16@gmail.com>
2026-08-04 21:46:22 -0400

    Improve inventory synchronization for CI

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,5 +1,8 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
+from config import CART_URL
 from utils.waits import wait_for_clickable, wait_for_visible
 
 
@@ -32,6 +35,15 @@ class InventoryPage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+    def wait_for_cart_count(self, expected_count):
+        self.wait.until(
+            EC.text_to_be_present_in_element(
+                self.CART_BADGE,
+                str(expected_count),
+            )
+        )
 
     def add_backpack(self):
         wait_for_clickable(
@@ -39,11 +51,15 @@ class InventoryPage:
             self.BACKPACK_ADD_BUTTON,
         ).click()
 
+        self.wait_for_cart_count("1")
+
     def add_bike_light(self):
         wait_for_clickable(
             self.driver,
             self.BIKE_LIGHT_ADD_BUTTON,
         ).click()
+
+        self.wait_for_cart_count("2")
 
     def add_bolt_tshirt(self):
         wait_for_clickable(
@@ -51,17 +67,29 @@ class InventoryPage:
             self.BOLT_TSHIRT_ADD_BUTTON,
         ).click()
 
+        self.wait_for_cart_count("3")
+
     def remove_backpack(self):
         wait_for_clickable(
             self.driver,
             self.BACKPACK_REMOVE_BUTTON,
         ).click()
 
+        self.wait.until(
+            EC.invisibility_of_element_located(
+                self.CART_BADGE
+            )
+        )
+
     def open_cart(self):
         wait_for_clickable(
             self.driver,
             self.CART_LINK,
         ).click()
+
+        self.wait.until(
+            EC.url_to_be(CART_URL)
+        )
 
     def get_cart_count(self):
         return wait_for_visible(
@@ -70,6 +98,8 @@ class InventoryPage:
         ).text
 
     def is_cart_badge_removed(self):
-        return len(
-            self.driver.find_elements(*self.CART_BADGE)
-        ) == 0
+        return self.wait.until(
+            EC.invisibility_of_element_located(
+                self.CART_BADGE
+            )
+        )
