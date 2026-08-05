2dbd1a8ebca59e39ac7ac8f5a6848348af87e24a
Charvita-vali <charvita16@gmail.com>
2026-08-04 23:21:08 -0400

    Update inventory_page.py

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 from config import CART_URL
 from utils.waits import (
-    wait_for_clickable,
+    reliable_click,
     wait_for_invisible,
     wait_for_text,
     wait_for_url,
@@ -13,20 +13,20 @@ from utils.waits import (
 class InventoryPage:
 
     BACKPACK_ADD_BUTTON = (
-        By.ID,
-        "add-to-cart-sauce-labs-backpack",
+        By.CSS_SELECTOR,
+        "[data-test='add-to-cart-sauce-labs-backpack']",
     )
     BIKE_LIGHT_ADD_BUTTON = (
-        By.ID,
-        "add-to-cart-sauce-labs-bike-light",
+        By.CSS_SELECTOR,
+        "[data-test='add-to-cart-sauce-labs-bike-light']",
     )
     BOLT_TSHIRT_ADD_BUTTON = (
-        By.ID,
-        "add-to-cart-sauce-labs-bolt-t-shirt",
+        By.CSS_SELECTOR,
+        "[data-test='add-to-cart-sauce-labs-bolt-t-shirt']",
     )
     BACKPACK_REMOVE_BUTTON = (
-        By.ID,
-        "remove-sauce-labs-backpack",
+        By.CSS_SELECTOR,
+        "[data-test='remove-sauce-labs-backpack']",
     )
     CART_BADGE = (
         By.CSS_SELECTOR,
@@ -35,6 +35,75 @@ class InventoryPage:
     CART_LINK = (
         By.CSS_SELECTOR,
         "[data-test='shopping-cart-link']",
+    )
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def add_backpack(self):
+        reliable_click(
+            self.driver,
+            self.BACKPACK_ADD_BUTTON,
+        )
+        wait_for_text(
+            self.driver,
+            self.CART_BADGE,
+            "1",
+        )
+
+    def add_bike_light(self):
+        reliable_click(
+            self.driver,
+            self.BIKE_LIGHT_ADD_BUTTON,
+        )
+        wait_for_text(
+            self.driver,
+            self.CART_BADGE,
+            "2",
+        )
+
+    def add_bolt_tshirt(self):
+        reliable_click(
+            self.driver,
+            self.BOLT_TSHIRT_ADD_BUTTON,
+        )
+        wait_for_text(
+            self.driver,
+            self.CART_BADGE,
+            "3",
+        )
+
+    def remove_backpack(self):
+        reliable_click(
+            self.driver,
+            self.BACKPACK_REMOVE_BUTTON,
+        )
+        wait_for_invisible(
+            self.driver,
+            self.CART_BADGE,
+        )
+
+    def open_cart(self):
+        reliable_click(
+            self.driver,
+            self.CART_LINK,
+        )
+        wait_for_url(
+            self.driver,
+            CART_URL,
+        )
+
+    def get_cart_count(self):
+        return wait_for_visible(
+            self.driver,
+            self.CART_BADGE,
+        ).text
+
+    def is_cart_badge_removed(self):
+        return wait_for_invisible(
+            self.driver,
+            self.CART_BADGE,
+        )        "[data-test='shopping-cart-link']",
     )
 
     def __init__(self, driver):
