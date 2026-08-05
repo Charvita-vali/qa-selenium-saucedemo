b5b847846feaac5ceb067202bf669a8635f0730e
Charvita-vali <charvita16@gmail.com>
2026-08-04 23:20:48 -0400

    Update waits.py

--- utils/waits.py
+++ utils/waits.py
@@ -2,42 +2,81 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-DEFAULT_TIMEOUT = 15
+DEFAULT_TIMEOUT = 20
 
 
 def wait_for_visible(driver, locator, timeout=DEFAULT_TIMEOUT):
-    """Wait until an element is visible and return it."""
     return WebDriverWait(driver, timeout).until(
         EC.visibility_of_element_located(locator)
     )
 
 
-def wait_for_clickable(driver, locator, timeout=DEFAULT_TIMEOUT):
-    """Wait until an element is clickable and return it."""
+def wait_for_present(driver, locator, timeout=DEFAULT_TIMEOUT):
     return WebDriverWait(driver, timeout).until(
-        EC.element_to_be_clickable(locator)
+        EC.presence_of_element_located(locator)
     )
 
 
 def wait_for_invisible(driver, locator, timeout=DEFAULT_TIMEOUT):
-    """Wait until an element disappears."""
     return WebDriverWait(driver, timeout).until(
         EC.invisibility_of_element_located(locator)
     )
 
 
 def wait_for_text(driver, locator, text, timeout=DEFAULT_TIMEOUT):
-    """Wait until an element contains the expected text."""
     return WebDriverWait(driver, timeout).until(
-        EC.text_to_be_present_in_element(
-            locator,
-            str(text),
-        )
+        EC.text_to_be_present_in_element(locator, str(text))
+    )
+
+
+def wait_for_value(driver, locator, value, timeout=DEFAULT_TIMEOUT):
+    return WebDriverWait(driver, timeout).until(
+        EC.text_to_be_present_in_element_value(locator, value)
     )
 
 
 def wait_for_url(driver, expected_url, timeout=DEFAULT_TIMEOUT):
-    """Wait until the browser reaches the expected URL."""
     return WebDriverWait(driver, timeout).until(
         EC.url_to_be(expected_url)
     )
+
+
+def reliable_click(driver, locator, timeout=DEFAULT_TIMEOUT):
+    element = wait_for_present(driver, locator, timeout)
+
+    driver.execute_script(
+        "arguments[0].scrollIntoView({block: 'center'});",
+        element,
+    )
+
+    driver.execute_script(
+        "arguments[0].click();",
+        element,
+    )
+
+
+def set_input_value(driver, locator, value, timeout=DEFAULT_TIMEOUT):
+    element = wait_for_visible(driver, locator, timeout)
+
+    driver.execute_script(
+        """
+        const element = arguments[0];
+        const value = arguments[1];
+        const setter = Object.getOwnPropertyDescriptor(
+            window.HTMLInputElement.prototype,
+            'value'
+        ).set;
+
+        setter.call(element, value);
+        element.dispatchEvent(
+            new Event('input', { bubbles: true })
+        );
+        element.dispatchEvent(
+            new Event('change', { bubbles: true })
+        );
+        """,
+        element,
+        value,
+    )
+
+    wait_for_value(driver, locator, value, timeout)
