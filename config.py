49eebc1074d5a3e261d1d75aa96c70e4ad0a7e22
Charvita-vali <charvita16@gmail.com>
2026-08-04 20:58:37 -0400

    Add shared test configuration

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
+BASE_URL = "https://www.saucedemo.com/"
 
+STANDARD_USER = "standard_user"
+LOCKED_OUT_USER = "locked_out_user"
+
+PASSWORD = "secret_sauce"
+INVALID_PASSWORD = "wrong_password"
+
+INVENTORY_URL = "https://www.saucedemo.com/inventory.html"
+CART_URL = "https://www.saucedemo.com/cart.html"
+CHECKOUT_STEP_ONE_URL = "https://www.saucedemo.com/checkout-step-one.html"
+CHECKOUT_STEP_TWO_URL = "https://www.saucedemo.com/checkout-step-two.html"
+CHECKOUT_COMPLETE_URL = "https://www.saucedemo.com/checkout-complete.html"
