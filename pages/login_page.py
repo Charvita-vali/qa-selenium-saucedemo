aeb4c5b158fb9bcbe1258f65da93dc942a492a3e
Charvita-vali <charvita16@gmail.com>
2026-08-04 21:06:39 -0400

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,7 +1,11 @@
 from selenium.webdriver.common.by import By
 
 from config import BASE_URL
-from utils.waits import wait_for_clickable, wait_for_url
+from utils.waits import (
+    wait_for_clickable,
+    wait_for_url,
+    wait_for_visible,
+)
 
 
 class LoginPage:
@@ -9,6 +13,7 @@ class LoginPage:
     USERNAME = (By.ID, "user-name")
     PASSWORD = (By.ID, "password")
     LOGIN_BUTTON = (By.ID, "login-button")
+    ERROR_MESSAGE = (By.CSS_SELECTOR, "[data-test='error']")
 
     def __init__(self, driver):
         self.driver = driver
@@ -22,3 +27,14 @@ class LoginPage:
         wait_for_clickable(self.driver, self.LOGIN_BUTTON).click()
 
         wait_for_url(self.driver, expected_url)
+    def login_without_url_wait(self, username, password):
+    wait_for_clickable(self.driver, self.USERNAME).send_keys(username)
+    wait_for_clickable(self.driver, self.PASSWORD).send_keys(password)
+    wait_for_clickable(self.driver, self.LOGIN_BUTTON).click()
+
+
+    def get_error_message(self):
+        return wait_for_visible(
+            self.driver,
+            self.ERROR_MESSAGE,
+        ).text
