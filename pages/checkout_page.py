ed898ce9a825e3b5b9ab72dc3945b667613b9ec3
Charvita-vali <charvita16@gmail.com>
2026-08-04 23:21:43 -0400

    Update checkout_page.py

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -5,7 +5,8 @@ from config import (
     CHECKOUT_STEP_TWO_URL,
 )
 from utils.waits import (
-    wait_for_clickable,
+    reliable_click,
+    set_input_value,
     wait_for_url,
     wait_for_visible,
 )
@@ -45,53 +46,46 @@ class CheckoutPage:
     def __init__(self, driver):
         self.driver = driver
 
-    def fill_field(self, locator, value):
-        field = wait_for_visible(
-            self.driver,
-            locator,
-        )
-        field.clear()
-        field.send_keys(value)
-
     def enter_checkout_information(
         self,
         first_name,
         last_name,
         postal_code,
     ):
-        self.fill_field(
+        set_input_value(
+            self.driver,
             self.FIRST_NAME,
             first_name,
         )
-        self.fill_field(
+        set_input_value(
+            self.driver,
             self.LAST_NAME,
             last_name,
         )
-        self.fill_field(
+        set_input_value(
+            self.driver,
             self.POSTAL_CODE,
             postal_code,
         )
 
     def click_continue(self):
-        wait_for_clickable(
+        reliable_click(
             self.driver,
             self.CONTINUE_BUTTON,
-        ).click()
+        )
 
     def continue_to_overview(self):
         self.click_continue()
-
         wait_for_url(
             self.driver,
             CHECKOUT_STEP_TWO_URL,
         )
 
     def finish_order(self):
-        wait_for_clickable(
+        reliable_click(
             self.driver,
             self.FINISH_BUTTON,
-        ).click()
-
+        )
         wait_for_url(
             self.driver,
             CHECKOUT_COMPLETE_URL,
